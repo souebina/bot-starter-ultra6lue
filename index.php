f6fce2126ef018814448f94bdb70bfd338a3c12b
ultra6lue <ultra6lue@gmail.com>
2017-04-14 22:03:28 +0900

    Bug fix

--- index.php
+++ index.php
@@ -91,7 +91,6 @@ replyConfirmTemplate($bot,
     new LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder (
       "非表示", "never")
     );
-}
 
 # 1.テキストメッセージの送信には、Bot->replyText関数が手軽ですが、他のメッセージと合わせて送ることも考慮しTextMessageBuilderを使って送信しましょう
 function replyTextMessage($bot, $replyToken, $text) {
